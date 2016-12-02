6abb0b0c81bfa5935780ec16e08f90775a025e03
tony skidmore <anthony.skidmore-accenture.com@workstation-3673.rhpds.opentlc.com>
2016-12-02 05:47:55 -0500

    correcting screen commands

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-sudo yum -y http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum -y install ansible git screen
-screen -d -R shared
 git clone https://github.com/tonyskidmore/redhat-director-8-lab.git
 cd redhat-director-8-lab
+
 touch ansible.log
 sudo ansible-playbook setup.yml
-ansible-playbook site.yml
+read -p "Press any key to begin installation..."
 
+screen -S "shared" -d -m
+screen -r "shared" -X stuff $'cd redhat-director-8-lab\n'
+screen -r "shared" -X stuff $'ansible-playbook site.yml\n'
+screen -x "shared"
