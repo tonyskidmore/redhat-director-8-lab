57d0bbcee3320516c620e73116de04f63cac3e07
Skidmore <anthony.skidmore@accenture.com>
2016-12-04 09:58:29 +0000

    enabling debug

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ git clone https://github.com/tonyskidmore/redhat-director-8-lab.git
 cd redhat-director-8-lab
 
 touch ansible.log
-sudo ansible-playbook setup.yml
+sudo ansible-playbook setup.yml -vv
 # read -p "Press any key to begin installation..."
 
 screen -S "shared" -d -m
