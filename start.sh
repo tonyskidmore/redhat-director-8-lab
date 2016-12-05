ab106586ec1d4177f0d969b7a75d00ffa5d4e282
tony skidmore <anthony.skidmore-accenture.com@workstation-7459.rhpds.opentlc.com>
2016-12-05 10:28:50 -0500

    amending debug output

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ git clone https://github.com/tonyskidmore/redhat-director-8-lab.git
 cd redhat-director-8-lab
 
 touch ansible.log
-sudo ansible-playbook setup.yml -vv
+sudo ansible-playbook setup.yml
 # read -p "Press any key to begin installation..."
 
 screen -S "shared" -d -m
